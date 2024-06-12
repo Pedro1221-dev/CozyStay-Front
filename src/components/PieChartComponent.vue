ecfd7c2619ea998db6ae00b6375f4121514f0632
diogojvale <diogojvale@gmail.com>
2024-06-12 09:27:58 +0100

    Create PieChartComponent.vue

---
+++ src/components/PieChartComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <Pie :data="data" :options="options" />
+</template>
+  
+<script lang="ts">
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Pie } from 'vue-chartjs'
+import * as chartConfig from '../components/chartConfig.js'
+  
+ChartJS.register(ArcElement, Tooltip, Legend)
+  
+export default {
+    name: 'App',
+    components: {
+        Pie
+    },
+    data() {
+        return chartConfig
+    }
+}
+</script>
+
+
+
+  
